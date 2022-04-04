e9cfae2521d99ff14d7e13aecd102bd0f30954fc
nour chouchene <chouchenebannour@gmail.com>
2022-04-04 17:21:03 +0100

    data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,4 +8,11 @@ templateUrl : 'server.component.html'
 
 export class ServerComponent {
 
+    serverId : number = 10 ;
+    serverStatus : string = 'offline';
+
+    getServerStatus () {
+
+        return this.serverStatus ; 
+    }
 }
